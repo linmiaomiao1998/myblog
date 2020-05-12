ffa207c618143207a0458b6b5cfad4763563c087
linmiaomiao1998 <994717247@qq.com>
2020-05-12 16:25:02 +0800

    增删改查

---
+++ express-sticky-note/models/note.js
@@ -0,0 +1,30 @@
+
+var Sequelize = require('sequelize');
+var path = require('path');
+
+var sequelize = new Sequelize(undefined,undefined, undefined, {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  // SQLite only
+  storage: path.join(__dirname, '../database/database.sqlite') 
+});
+
+var Note = sequelize.define('note', {
+    text: {
+      type: Sequelize.STRING
+    },
+    username: {
+      type: Sequelize.STRING
+    }
+  });
+/*
+  Note.sync().then(function() { 
+    Note.create({text:'hello'})
+   }).then(function(){
+    Note.findAll().then(function(notes){
+        console.log(notes)
+   })
+});
+*/
+module.exports.Note=Note;
