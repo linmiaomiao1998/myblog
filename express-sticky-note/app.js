3081539c7769451a2ac3a5ed8f626d6ed11f1bff
linmiaomiao1998 <994717247@qq.com>
2020-05-19 21:21:48 +0800

    login

--- express-sticky-note/app.js
+++ express-sticky-note/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var passport = require('passport');
+var session = require('express-session');
 var index = require('./routes/index');
 var api = require('./routes/api');
+var auth = require('./routes/auth');
 
 var app = express();
 
@@ -18,9 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'sessionsecret'}));///中间件，sessionsecret：密钥，可更改
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/api', api);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- express-sticky-note/routes/index.js
+++ express-sticky-note/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.query)
+  var loginData;
+  if(req.session.user){
+    loginData = {
+      isLogin: true,
+      user:{
+        avatar:req.session.user.avatar,
+        username:req.session.user.username
+      } 
+    }
+  }else{
+    loginData = {
+      isLogin: false
+    }
+  }
+
+  res.render('index', loginData);
 });
 
 module.exports = router;
