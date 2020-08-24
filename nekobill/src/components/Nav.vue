b8f520d0095b7f49d78f8ec8b203ebf1d8d57688
linmiaomiao1998 <994717247@qq.com>
2020-08-24 22:26:44 +0800

    创建icon

--- nekobill/src/components/Nav.vue
+++ nekobill/src/components/Nav.vue
@@ -2,21 +2,22 @@ import Nav from '@/components/Nav.vue';
 <template>
       <div class="nav">
       <router-link to="/money">
-      <svg>
-        <use xlink:href="#label"></use>
-      </svg>
+      <Icon name="money"/>
       记账
       </router-link>
       |
-      <router-link to="/labels">标签</router-link>
+      <router-link to="/labels">
+      <Icon name="label"/>
+      标签</router-link>
       |
-      <router-link to="/statistics">统计</router-link>
+      <router-link to="/statistics">
+         <Icon name="statistics"/>
+      统计</router-link>
     </div>
 </template>
 
 <script lang="ts">
-import  x from '@/assets/icons/label.svg';
-console.log(x);
+
 export default {
   name:'Nav'
 }
