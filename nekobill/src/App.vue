315e2faa48a44b22b6fd1f312e0f257ed3145240
linmiaomiao1998 <994717247@qq.com>
2020-08-26 09:48:21 +0800

    添加nav样式

--- nekobill/src/App.vue
+++ nekobill/src/App.vue
@@ -14,6 +14,10 @@
 body{
   line-height: 1.5;
 }
+a{
+  text-decoration: none;
+  color: inherit;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- nekobill/src/components/Nav.vue
+++ nekobill/src/components/Nav.vue
@@ -23,17 +23,25 @@ export default {
 }
 </script>
 
+
 <style lang="scss" scoped>
-.nav{
-  display: flex;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
-   flex-direction: row;
- > .item{
-    width:33.33333%;
+  nav {
     display: flex;
-    align-items: center;
+    box-shadow:0 0 3px rgba(0, 0, 0, 0.25);
+    flex-direction: row;
+    font-size: 12px;
+  > .item {
+    width: 33.33333%;
+    display: flex;
     justify-content: center;
+    align-items: center;
     flex-direction: column;
-}
-}
+    padding: 2px 0;
+    .icon{
+      width: 32px;
+      height: 32px;
+    }
+   }
+  } 
+
 </style>
