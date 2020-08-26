2d126a99617a0a02b5470507a6dd60f509629e7c
linmiaomiao1998 <994717247@qq.com>
2020-08-26 15:46:45 +0800

    active class 更改

--- nekobill/src/components/Nav.vue
+++ nekobill/src/components/Nav.vue
@@ -1,16 +1,17 @@
 import Nav from '@/components/Nav.vue';
+import { RouteConfig from 'vue-router';
 <template>
       <nav>
-      <router-link to="/money" class="item">
+      <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
       </router-link>
       |
-      <router-link to="/labels" class="item">
+      <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签</router-link>
       |
-      <router-link to="/statistics" class="item">
+      <router-link to="/statistics" class="item" active-class="selected">
          <Icon name="statistics"/>
       统计</router-link>
       </nav>
@@ -37,11 +38,38 @@ export default {
     align-items: center;
     flex-direction: column;
     padding: 2px 0;
+    color: #A06225;
     .icon{
       width: 32px;
       height: 32px;
+      
     }
    }
-  } 
-
+   > .item.selected{
+     text-align: center;
+            padding: 2px;
+            margin: 0;
+            font-family: helvetica, arial, sans-serif;
+            font-size: 4px;
+            font-weight: bold;
+            color:#F2DAA8;
+            /*background-color: #F0CC82;
+    
+            /*text-shadow: 1px 1px white,-1px -1px #333;*/
+            /*text-shadow: -1px -1px white,1px 1px #333;*/
+           text-shadow: -1px 0 #A06225,1px 0#A06225,0 1px #A06225,0 -1px #A06225;
+           animation:selectedSlide 0.5s;
+   } 
+    } 
+@keyframes selectedSlide{
+  0%{
+    color:#A06225;
+    }
+    30%{ 
+      color:wheat;
+    }
+  100%{
+   color:#F2DAA8;
+  }
+}
 </style>
