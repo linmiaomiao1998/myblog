d94145948544e77907354e82d24b19ea6bb4809f
linmiaomiao1998 <994717247@qq.com>
2020-11-02 23:10:21 +0800

    修复Money.vue的CSS

--- nekobill/src/components/Money/Numberpad.vue
+++ nekobill/src/components/Money/Numberpad.vue
@@ -30,39 +30,40 @@
   export default class Types extends Vue {
     output: string = "";
     inputContent(event: MouseEvent) {
-      const button = (event.target as HTMLButtonElement);
+      const button = event.target as HTMLButtonElement;
       const input = button.textContent as string;
-      if(this.output.length===16){return;}
-      if(this.output ==='0'){
-        if('0123456789'.indexOf(input)>=0)
-        {
-          this.output=input;
-        }else{
-        this.output+=input;
-      }  
-      return;
+      if (this.output.length === 16) {
+        return;
       }
-       if(this.output.indexOf('.')>=0 && input==='.'){
-         this.output='0'+'.';
-         this.output=this.output.slice(0,-1);
-       }
-        this.output+=input;
-     }
-     remove(){
-       if(this.output.length===1){
-         this.output='0';
-       }else{
-         this.output=this.output.slice(0,-1);
-       }
-     }
-     clear(){
-       this.output='0'
-     }
-     ok(){
-       this.$emit('update:value',this.output)
-       this.$emit('submit',this.output)
-       this.output='0'
-     }
+      if (this.output === "0") {
+        if ("0123456789".indexOf(input) >= 0) {
+          this.output = input;
+        } else {
+          this.output += input;
+        }
+        return;
+      }
+      if (this.output.indexOf(".") >= 0 && input === ".") {
+        this.output = "0" + ".";
+        this.output = this.output.slice(0, -1);
+      }
+      this.output += input;
+    }
+    remove() {
+      if (this.output.length === 1) {
+        this.output = "0";
+      } else {
+        this.output = this.output.slice(0, -1);
+      }
+    }
+    clear() {
+      this.output = "0";
+    }
+    ok() {
+      this.$emit("update:value", this.output);
+      this.$emit("submit", this.output);
+      this.output = "0";
+    }
   }
 </script>
 
@@ -84,7 +85,7 @@
       @extend %clearfix;
       font-family: $font-hei;
       > button {
-        color:  #A06225;
+        color: #a06225;
         width: 25%;
         height: 64px;
         float: left;
@@ -95,38 +96,33 @@
           float: right;
         }
         $bg: #f6ba72;
-        &:nth-child(15),
-        &:nth-child(19) {
+
+        &:nth-child(12),
+        &:nth-child(15) {
           background: $bg;
         }
-        &:nth-child(18),
-        &:nth-child(14),
-        &:nth-child(10) {
-          background: lighten($bg, 4%);
-        }
-        &:nth-child(5),
-        &:nth-child(9),
-        &:nth-child(13),
-        &:nth-child(17) {
-          background: lighten($bg, 4% * 2);
+        &:nth-child(8),
+        &:nth-child(11),
+        &:nth-child(14) {
+          background: lighten($bg, 5% * 1);
         }
         &:nth-child(4),
-        &:nth-child(8),
-        &:nth-child(12),
-        &:nth-child(16) {
-          background: lighten($bg, 4% * 3);
+        &:nth-child(7),
+        &:nth-child(13),
+        &:nth-child(10) {
+          background: lighten($bg, 5% * 2);
         }
         &:nth-child(3),
-        &:nth-child(7),
-        &:nth-child(11) {
-          background: lighten($bg, 4% * 4);
+        &:nth-child(6),
+        &:nth-child(9) {
+          background: lighten($bg, 5% * 3);
         }
         &:nth-child(2),
-        &:nth-child(6) {
-          background: lighten($bg, 4% * 5);
+        &:nth-child(5) {
+          background: lighten($bg, 5% * 4);
         }
         &:nth-child(1) {
-          background: lighten($bg, 4% * 6);
+          background: lighten($bg, 5% * 5);
         }
       }
     }
