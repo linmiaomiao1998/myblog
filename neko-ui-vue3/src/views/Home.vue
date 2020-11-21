3900827eb1f32f814bdf7dcbf02b7e2864fb1f82
linmiaomiao1998 <994717247@qq.com>
2020-11-21 20:50:07 +0800

    aside CSS

--- neko-ui-vue3/src/views/Home.vue
+++ neko-ui-vue3/src/views/Home.vue
@@ -4,8 +4,8 @@
       <h1>neko UI</h1>
       <h2>一个随意的UI框架</h2>
       <p class="action">
-      <a>Github</a>
-      <a>开始</a>
+      <a href=" ">Github</a>
+      <router-link to="/doc">开始</router-link>
       </p>
     </div>
 </template>
@@ -19,5 +19,25 @@ import Topnav from "../components/Topnav.vue"
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+  }
+   a{
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
 </style>
