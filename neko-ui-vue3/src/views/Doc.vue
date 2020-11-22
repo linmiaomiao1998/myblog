7d3dbdd5a89cd40f380af19b10b454c75366caf6
linmiaomiao1998 <994717247@qq.com>
2020-11-22 22:04:58 +0800

    基本完成官网

--- neko-ui-vue3/src/views/Doc.vue
+++ neko-ui-vue3/src/views/Doc.vue
@@ -1,29 +1,30 @@
 <template>
   <div>
     <Topnav />
-      <div class="layout">
-    <Topnav class="nav" />
-    <div class="content">
-      <aside v-if="asideVisible">
-        <h2>组件列表</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/switch">Switch 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/button">Button 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/dialog">Dialog 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/tabs">Tabs 组件</router-link>
-          </li>
-        </ol>
-      </aside>
-      <main>
-      <router-view/>
-      </main>
+    <div class="layout">
+      <Topnav class="nav" />
+      <div class="content">
+        <aside v-if="asideVisible">
+          <h2>组件列表</h2>
+          <ol>
+            <li>
+              <router-link to="/doc/switch">Switch 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/button">Button 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/dialog">Dialog 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/tabs">Tabs 组件</router-link>
+            </li>
+          </ol>
+        </aside>
+        <main>
+          <router-view />
+        </main>
+      </div>
     </div>
   </div>
 </template>
@@ -42,33 +43,33 @@
 </script>
 
 <style lang="scss" scoped>
-.layout {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  > .nav {
-    flex-shrink: 0;
-  }
-  > .content {
-    flex-grow: 1;
-    padding-top: 60px;
-    padding-left: 156px;
-    @media (max-width: 500px) {
-      padding-left: 0; 
+  .layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    > .nav {
+      flex-shrink: 0;
+    }
+    > .content {
+      flex-grow: 1;
+      padding-top: 60px;
+      padding-left: 156px;
+      @media (max-width: 500px) {
+        padding-left: 0;
+      }
     }
   }
-}
-.content {
-  display: flex;
-  > aside {
-    flex-shrink: 0;
+  .content {
+    display: flex;
+    > aside {
+      flex-shrink: 0;
+    }
+    > main {
+      flex-grow: 1;
+      padding: 16px;
+      background: lightgreen;
+    }
   }
-  > main {
-    flex-grow: 1;
-    padding: 16px;
-    background: lightgreen;
-  }
-}
   aside {
     padding: 16px;
     background: lightblue;
@@ -87,7 +88,7 @@
       }
     }
     main {
-  overflow: auto;
-}
+      overflow: auto;
+    }
   }
 </style>
