10450e41fe02dced8588a211796ee4cc0f525a58
linmiaomiao1998 <994717247@qq.com>
2020-11-23 00:39:21 +0800

    可以切换switch

--- neko-ui-vue3/src/lib/Switch.vue
+++ neko-ui-vue3/src/lib/Switch.vue
@@ -1,11 +1,18 @@
 <template>
-  <button>
-    <span></span>
-  </button>
+   <button @click="toggle" :class="{checked}"> <span></span> </button>
 </template>
 
 <script>
-  export default {};
+  import { ref } from "vue";
+  export default {
+    setup() {
+     const checked = ref(false)
+    const toggle = ()=>{
+      checked.value = !checked.value
+    }
+    return {checked, toggle}
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
@@ -15,7 +22,7 @@
     height: $h;
     width: $h * 2;
     border: none;
-    background: blue;
+    background: gray;
     border-radius: $h/2;
     position: relative;
   }
@@ -28,7 +35,10 @@
     background: white;
     border-radius: $h2 / 2;
   }
- button:hover > span {
+  button.checked {
+    background: blue;
+  }
+  button.checked > span {
     left: calc(100% - #{$h2} - 2px);
   }
 </style>
