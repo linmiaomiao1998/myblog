26a318a5c575096ef1aa8651bf3d729a35bbfdf8
linmiaomiao1998 <994717247@qq.com>
2020-11-24 20:35:56 +0800

    增加size属性示例

--- neko-ui-vue3/src/lib/Switch.vue
+++ neko-ui-vue3/src/lib/Switch.vue
@@ -1,5 +1,10 @@
 <template>
-  <button @click="toggle" :class="{ checked: value }"><span></span></button>
+  <button
+    class="neko-switch"
+    @click="toggle"
+    :class="{ 'neko-checked': value }">
+    <span></span>
+  </button>
 </template>
 
 <script>
@@ -17,45 +22,23 @@
   };
 </script>
 
-<style lang="scss" scoped>
-  $h: 22px;
-  $h2: $h - 4px;
-  button {
-    height: $h;
-    width: $h * 2;
-    border: none;
-    background: #bfbfbf;
-    border-radius: $h/2;
-    position: relative;
-  }
-  span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background: white;
-    border-radius: $h2 / 2;
-    transition: all 250ms;
-  }
-  button.checked {
-    background: #1890ff;
-  }
-  button.checked > span {
-    left: calc(100% - #{$h2} - 2px);
-  }
-  button:focus {
-    outline: none;
-  }
-  button:active {
+<style lang="scss">
+    $h: 22px;
+    $h2: $h - 4px;
+    .neko-switch {
+    height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
     > span {
-      width: $h2 + 4px;
+      position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
     }
-  }
-  button.checked:active {
-    > span {
-      width: $h2 + 4px;
-      margin-left: -4px;
+    &.neko-checked { background: #1890ff;
+      > span { left: calc(100% - #{$h2} - 2px); }
+    }
+    &:focus { outline: none; }
+    &:active {
+      > span { width: $h2 + 4px; }
+    }
+    &.neko-checked:active {
+      > span { width: $h2 + 4px; margin-left: -4px; }
     }
   }
 </style>
