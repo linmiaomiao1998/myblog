dd62d1f1260c491ea7a9c7702ec537f540ef2327
linmiaomiao1998 <994717247@qq.com>
2020-11-24 16:07:57 +0800

    完善button组件

--- neko-ui-vue3/src/components/ButtonDemo.vue
+++ neko-ui-vue3/src/components/ButtonDemo.vue
@@ -1,10 +1,21 @@
 <template>
    <div>Button页面</div>
+   <Button @click="onClick"
+   @focus="onClick"
+   @mouseover="onClick"
+   >hello</Button>
 </template>
 
 <script>
+import Button from "../lib/Button.vue"
 export default {
-
+    components:{Button},
+    setup(){
+       const onClick = () =>{
+          console.log('hi')
+       }
+       return {onClick}
+    }
 }
 </script>
 
