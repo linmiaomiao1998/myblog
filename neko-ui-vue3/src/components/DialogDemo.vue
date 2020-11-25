f1cc25650dd2aadc0de2a0e6795215e39de233ca
linmiaomiao1998 <994717247@qq.com>
2020-11-25 22:43:47 +0800

    使用slot，并支持关闭

--- neko-ui-vue3/src/components/DialogDemo.vue
+++ neko-ui-vue3/src/components/DialogDemo.vue
@@ -1,18 +1,35 @@
 <template>
-   <div>Dialog页面</div>
-   <Dialog />
+  <div>Dialog页面</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x" :OnClickOverlay="false">
+    <template v-slot:content>
+      <div>hi</div>
+    </template>
+    <template v-slot:title>
+    <div>标题</div>
+    </template>
+  </Dialog>
 </template>
 
 <script lang="ts">
-import Dialog from "../lib/Dialog.vue"
-export default {
-   components:{
-      Dialog
-   }
+  import Dialog from "../lib/Dialog.vue";
+  import { ref } from "vue";
+  import Button from "../lib/Button.vue";
 
-}
+  export default {
+    components: {
+      Dialog,
+      Button,
+    },
+    setup() {
+      const x = ref(false);
+      const toggle = () => {
+        x.value = !x.value;
+      };
+      return { x, toggle };
+    },
+  };
 </script>
 
-<style>
-
-</style>
+<style></style>
